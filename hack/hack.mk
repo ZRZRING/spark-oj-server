52a28858b97d6956a055e4a96bff46694c3ffff0
zrzring <zrzring@qq.com>
2026-03-27 16:17:21 +0800

    fix: 清理没用的make命令

--- hack/hack.mk
+++ hack/hack.mk
@@ -30,40 +30,6 @@ enums: cli.install
 service: cli.install
 	@gf gen service
 
-
-# Build docker image.
-.PHONY: image
-image: cli.install
-	$(eval _TAG  = $(shell git rev-parse --short HEAD))
-ifneq (, $(shell git status --porcelain 2>/dev/null))
-	$(eval _TAG  = $(_TAG).dirty)
-endif
-	$(eval _TAG  = $(if ${TAG},  ${TAG}, $(_TAG)))
-	$(eval _PUSH = $(if ${PUSH}, ${PUSH}, ))
-	@gf docker ${_PUSH} -tn $(DOCKER_NAME):${_TAG};
-
-
-# Build docker image and automatically push to docker repo.
-.PHONY: image.push
-image.push: cli.install
-	@make image PUSH=-p;
-
-
-# Deploy image and yaml to current kubectl environment.
-.PHONY: deploy
-deploy: cli.install
-	$(eval _TAG = $(if ${TAG},  ${TAG}, develop))
-
-	@set -e; \
-	mkdir -p $(ROOT_DIR)/temp/kustomize;\
-	cd $(ROOT_DIR)/manifest/deploy/kustomize/overlays/${_ENV};\
-	kustomize build > $(ROOT_DIR)/temp/kustomize.yaml;\
-	kubectl   apply -f $(ROOT_DIR)/temp/kustomize.yaml; \
-	if [ $(DEPLOY_NAME) != "" ]; then \
-		kubectl patch -n $(NAMESPACE) deployment/$(DEPLOY_NAME) -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"$(shell date +%s)\"}}}}}"; \
-	fi;
-
-
 # Parsing protobuf files and generating go files.
 .PHONY: pb
 pb: cli.install
